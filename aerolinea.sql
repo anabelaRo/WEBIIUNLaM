4e0a3edf128d66debedcd8df537b681088838603
Sobrile <juan_el_diez@hotmail.com>
2014-05-17 13:38:11 -0300

    Script de la BB.DD

---
+++ aerolinea.sql
@@ -0,0 +1,146 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.12
+-- http://www.phpmyadmin.net
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 17-05-2014 a las 18:06:09
+-- Versión del servidor: 5.5.36
+-- Versión de PHP: 5.4.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de datos: `aerolinea`
+--
+CREATE DATABASE IF NOT EXISTS `aerolinea` DEFAULT CHARACTER SET utf8 COLLATE utf8_spanish_ci;
+USE `aerolinea`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `avion`
+--
+
+CREATE TABLE IF NOT EXISTS `avion` (
+  `codigo` int(11) NOT NULL,
+  `asientos_primera` int(11) NOT NULL,
+  `asientos_economy` int(11) NOT NULL,
+  `columna_primera` int(11) NOT NULL,
+  `columna_economy` int(11) NOT NULL,
+  PRIMARY KEY (`codigo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci COMMENT='Tabla de Aviones';
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ciudad`
+--
+
+CREATE TABLE IF NOT EXISTS `ciudad` (
+  `cod_prov` int(11) NOT NULL,
+  `codigo` int(11) NOT NULL,
+  `descripcion` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  PRIMARY KEY (`codigo`),
+  KEY `cod_prov` (`cod_prov`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `forma_pago`
+--
+
+CREATE TABLE IF NOT EXISTS `forma_pago` (
+  `codigo` int(11) NOT NULL,
+  `descripcion` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  PRIMARY KEY (`codigo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci COMMENT='Tabla de Formas de Pago';
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pago`
+--
+
+CREATE TABLE IF NOT EXISTS `pago` (
+  `codigo` int(11) NOT NULL,
+  `cod_forma_pago` int(11) NOT NULL,
+  `nro_tarjeta` varchar(20) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `importe` decimal(6,2) NOT NULL,
+  PRIMARY KEY (`codigo`),
+  KEY `cod_forma_pago` (`cod_forma_pago`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci COMMENT='Tabla de Pagos';
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pasaje`
+--
+
+CREATE TABLE IF NOT EXISTS `pasaje` (
+  `cod_usuario` int(11) NOT NULL,
+  `cod_vuelo` int(11) NOT NULL,
+  `cod_pago` int(11) DEFAULT NULL,
+  `cod_reserva` int(11) NOT NULL,
+  `fecha_reserva` date NOT NULL,
+  `cod_asiento` varchar(4) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `flag_check_in` tinyint(1) NOT NULL,
+  PRIMARY KEY (`cod_reserva`),
+  KEY `cod_vuelo` (`cod_vuelo`,`cod_pago`,`cod_reserva`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `provincia`
+--
+
+CREATE TABLE IF NOT EXISTS `provincia` (
+  `codigo` int(11) NOT NULL,
+  `descripcion` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  PRIMARY KEY (`codigo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci COMMENT='Tabla de Provincias';
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE IF NOT EXISTS `usuario` (
+  `codigo` int(11) NOT NULL,
+  `dni` int(11) NOT NULL,
+  `nombre_apellido` varchar(60) COLLATE utf8_spanish_ci NOT NULL,
+  `fecha_nacimiento` date NOT NULL,
+  `email` varchar(50) COLLATE utf8_spanish_ci DEFAULT NULL,
+  PRIMARY KEY (`codigo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `vuelo`
+--
+
+CREATE TABLE IF NOT EXISTS `vuelo` (
+  `codigo` int(11) NOT NULL,
+  `origen` int(11) NOT NULL,
+  `destino` int(11) NOT NULL,
+  `fecha` date NOT NULL,
+  `hora` time NOT NULL,
+  `avion` int(11) NOT NULL,
+  PRIMARY KEY (`codigo`),
+  KEY `origen` (`origen`,`destino`,`avion`),
+  KEY `destino` (`destino`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci COMMENT='Tabla de Vuelos';
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
